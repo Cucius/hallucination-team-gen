724cecc9e5c3c1dfa60af5ed2cb89e7d09e723d2
Cucius <Rylan19@Gmail.com>
2022-09-02 02:31:31 -0600

    Added code to prompt for User input based on the employee position

--- index.js
+++ index.js
@@ -4,19 +4,120 @@ const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 
+// store team members
+let teamMembers = [];
+
+// flag to control questions
+let continueAddingMembers = true;
+
 // call rest of functions in order
 const start = async () => {
-  await promptUser();
+  await promptUser().then(() => {
+    console.log(teamMembers);
+    // generate html
+  });
 };
 
 // get answers from user
-const promptUser = async () => {};
+const promptUser = async () => {
+  let managerFlag = true;
+  let nextEmployee = "";
+
+  while (continueAddingMembers) {
+    const questions = [
+      {
+        type: "input",
+        message: `What is the name of the ${managerFlag ? "manager" : "employee"}?`,
+        name: "name",
+      },
+      {
+        type: "input",
+        message: `Enter the ${managerFlag ? "manager" : "employee"}'s ID:`,
+        name: "id",
+      },
+      {
+        type: "input",
+        message: `Enter the ${managerFlag ? "manager" : "employee"}'s email:`,
+        name: "email",
+      },
+    ];
+
+    const managerQuestions = questions;
+
+    managerQuestions.push({
+      type: "input",
+      message: "Enter the team manager's office number:",
+      name: "officeNumber",
+    });
+
+    const dealWithAnswers = async (answers) => {
+      let roleSpecificPrompts = "";
+
+      if (nextEmployee === "Engineer") {
+        roleSpecificPrompts = await inquirer.prompt([
+          {
+            type: "input",
+            message: "Enter the Employee's GitHub Username:",
+            name: "gitHub",
+          },
+        ]);
+
+        const engineer = new Engineer(answers.name, answers.id, answers.email, roleSpecificPrompts.gitHub);
+        teamMembers.push(engineer); // push new team member
+      } else if (nextEmployee === "Intern") {
+        roleSpecificPrompts = await inquirer.prompt([
+          {
+            type: "input",
+            message: "Enter the Employee's school:",
+            name: "school",
+          },
+        ]);
+
+        const intern = new Intern(answers.name, answers.id, answers.email, roleSpecificPrompts.school);
+        teamMembers.push(intern); // push new team member
+      } else if (nextEmployee === "No more team members") {
+        // flip control flag
+        continueAddingMembers = false;
+      }
+    };
+
+    if (managerFlag) {
+      const answers = await inquirer.prompt(managerQuestions);
+      managerFlag = false;
+
+      const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+      teamMembers.push(manager);
+    } else {
+      const answers = await inquirer.prompt(questions);
+
+      await dealWithAnswers(answers);
+    }
+
+    const continueOrNot = await inquirer.prompt([
+      {
+        type: "list",
+        message: "Add another Team Member",
+        choices: ["Engineer", "Intern", "No more team members"],
+        name: "role",
+      },
+    ]);
+
+    if (continueOrNot.role === "No more team members") {
+      continueAddingMembers = false;
+      nextEmployee = "No more team members";
+    } else if (continueOrNot.role === "Engineer") {
+      nextEmployee = "Engineer";
+    } else if (continueOrNot.role === "Intern") {
+      nextEmployee = "Intern";
+    }
+  }
+};
+
+start();
 
 /*
   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 */
-
-// prompt for role
 
 // use class to determine html
 
@@ -24,44 +125,3 @@ const promptUser = async () => {};
 // new employee -> new manager
 // employee.id employee.email .... manager.officeNumber
 //
-
-//Generate a webpage that displays Team's basic info
-
-// -- Command-line app that accepts user input
-// 	-prompt for team members and info
-
-// 	*HTML{
-// 		*FS
-// 		-generate an HTML file
-// 			*CSS/Bootstrap
-// 			-format HTML to display cleanly
-// 		-HTML links to email addresses
-// 			-mailto
-// 		-GitHub link opens in new tab
-// 			-target
-// 	}HTML*
-
-// 	-running the application on start command
-
-// 	*Inquirer{
-// 		-prompt for info
-// 			-There is only one Team Manager
-// 				-ask for name
-// 				-ask for employee ID
-// 				-ask for email address
-// 				-ask for office number
-// 			-Menu prompt to add more team members
-// 				-Engineer
-// 					-ask for name ..super
-// 					-ask for ID ..super
-// 					-ask for email ..super
-// 					-ask for GitHub username
-// 				-Intern
-// 					-ask for name ..super
-// 					-ask for ID ..super
-// 					-ask for email ..super
-// 					-ask for school
-// 				-No new team members
-// 					-generate the HTML with input
-// 					-exit the app
-// 	}Inquirer*
